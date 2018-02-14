1cbbe9f3d0f3e0c42045204036ef978cb5446f9a
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-02-14 14:58:22 +0530

    Update leap year.py

--- leap year.py
+++ leap year.py
@@ -1,5 +1,7 @@
-n=(int(input("enter the year:")))
-if n%4 ==0:
+a=(int(input("enter the year:")))
+if a<0:
+    print("invalid")
+elif a%4 ==0:
     print("the year is leap")
 else:
     print("the year is not leap")
