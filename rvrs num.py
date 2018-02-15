a44e580b13f1d03b53444b69c60cc9ef60cb364f
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-02-15 10:31:25 +0530

    Update rvrs num.py

--- rvrs num.py
+++ rvrs num.py
@@ -1,2 +1,7 @@
-num = input("insert a number of your choice ")
-print (num[::-1])
+n=int(input("Enter number: "))
+rev=0
+while(n>0):
+    dig=n%10
+    rev=rev*10+dig
+    n=n//10
+print("Reverse of the number:",rev)
