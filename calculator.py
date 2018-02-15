1dff650106aa89f6ccc02d33c0a7be0005a3b966
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-02-15 16:19:20 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,22 +1,22 @@
 m=float(input("enter the first number:"))
 n=float(input("enter the second number:"))
 s=int(input("select any operator: \n 1]add \n 2]sub \n 3]mul \n 4]div \n 5]rem \n 6]squ \n 7]pwr \n"))
-if (n==0):
-  print ("exception")
-elif(s==1):
+if(s==1):
   print (m+n)
 elif (s==2):
   print (m-n)
 elif (s==3):
   print (m*n)
-elif (s==4):
-  print (m/n)
 elif (s==5):
   print (m%n)
 elif (s==6):
   print (m**2)
 elif (s==7):
   print (m**n)
+elif (s==4):
+    if (n==0):
+        print ("exception")
+    else:
+        print (m/n)
 else:
-  print("\n invalidsss")
-  
+  print() 
