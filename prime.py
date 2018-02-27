d65d7520c19750cfd05c1212700dae8fad18f4a3
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-02-27 17:52:01 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -3,9 +3,9 @@ num = int(input("enter the number:"))
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
+           print(num,"no")
            break
    else:
-       print(num,"is a prime number")
+       print(num,"yes")
 else:
-   print(num,"is not a prime number")
+   print(num,"no")
