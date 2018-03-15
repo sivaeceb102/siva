12e3ecca869679331b07e16b04373990c24274fa
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-03-15 17:27:42 +0530

    Create hunter set1.3.py

---
+++ hunter set1.3.py
@@ -0,0 +1,11 @@
+def linearSearch(arr, n):
+    for i in range(n):
+        if arr[i] is i:
+            return i
+    
+    return -1
+ 
+
+arr = [-10, -1, 0, 2, 10, 11, 45, 50, 100]
+n = len(arr)
+print("Fixed Point is " + str(linearSearch(arr,n)))
