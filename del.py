08aae69ed46b1067d151380bf3b0a703f71aec9c
sivaeceb102 <36372983+sivaeceb102@users.noreply.github.com>
2018-03-15 17:13:56 +0530

    Create del.py

---
+++ del.py
@@ -0,0 +1,14 @@
+n =int(input("Enter the number:\n"))
+k=int(input("Enter the digit to delete:\n"))
+m= int(str(n)[:-k])
+print(m)
+def smallest(lst):
+    for i,n in enumerate(lst):
+        if n != '0':
+            tmp = lst.pop(i)
+            break
+    return str(tmp) + ''.join(lst)
+if __name__ == '__main__':
+    lst = list(str(m))
+    lst.sort()
+print ("smallest number combination is:\n",smallest(lst))
